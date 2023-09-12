d6b5b4bb34ca136c49c48e51ecfc00cad4e24b78
Scott Nusbaum <snus@local.com>
2023-09-11 21:23:55 -0400

    Corrected the IAT Parsing. Didn't iterate through the names to find the correct Address. Added a basic example of API hashing. Used a hash similar to that used by Metasploit

--- techniques/injectionTechniques/apiHashing/poc/evil.c
+++ techniques/injectionTechniques/apiHashing/poc/evil.c
@@ -11,52 +11,53 @@ int myFunction( HWND hWnd, LPCSTR lpText, LPCSTR lpCaption, UINT uType )
     (*msgbox)(hWnd, lpText, lpCaption, uType);
 
 }
-void* getIATAddress( char* libName, char* funcName )
-{
-    HMODULE hModule = GetModuleHandle(0);
-    PIMAGE_DOS_HEADER dosHeaders = (PIMAGE_DOS_HEADER)hModule;
-    PIMAGE_NT_HEADERS ntHeaders = (PIMAGE_NT_HEADERS)((DWORD_PTR)hModule+ dosHeaders->e_lfanew);
-    HANDLE tmpHookAddr = NULL;
-
-    // resolve import address table
-    IMAGE_DATA_DIRECTORY importsDirectory = ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
-    PIMAGE_IMPORT_DESCRIPTOR importDescriptor = (PIMAGE_IMPORT_DESCRIPTOR)(importsDirectory.VirtualAddress + (DWORD_PTR)hModule);
-    HANDLE library = NULL;
-    while (importDescriptor->Name != NULL)
-    {
-        LPCSTR libraryName = (LPCSTR)importDescriptor->Name + (DWORD_PTR)hModule;
-        if( strcmp(libraryName, libName) == 0)
-        {
-            library = importDescriptor->FirstThunk;
-            break;
-        }
-        importDescriptor++;
-    }
-    if( library == NULL) 
-    {
-        printf("Hook not found\n");
-        return;
-    }
-	HANDLE ptrFuncNameOffset = importDescriptor->Characteristics + (DWORD_PTR)hModule;
-	int funcNameOffset = (int)(*(int*)ptrFuncNameOffset);
-	int count = 0;
-	while( funcNameOffset != 0)
-	{
-		char* lFuncName = (char*)(*(int*)(ptrFuncNameOffset + count*8) + (DWORD_PTR)hModule+2);
-		if( strcmp( lFuncName, funcName) == 0) break;
-		count += 1;	
-		funcNameOffset = (int)(*(int*)ptrFuncNameOffset + count*8);
-	}
-    tmpHookAddr = (DWORD_PTR)hModule + (DWORD_PTR)library + count*8;
-	void *ret = (void*)tmpHookAddr;
-    printf("Found %s (%llX)\n", funcName, ret );
-	return ret;
-}
 
 void patch()
 {
     printf("In evil doing evil things\n");
-    PIMAGE_THUNK_DATA thunk = (PIMAGE_THUNK_DATA )getIATAddress("USER32.dll","MessageBoxA");
+    HANDLE hProcess = GetCurrentProcess();
+    HMODULE hModule = GetModuleHandle(0);
+    printf("hModule (%08llX)\n", hModule);
+    PIMAGE_DOS_HEADER dosHeaders = (PIMAGE_DOS_HEADER)hModule;
+    PIMAGE_NT_HEADERS ntHeaders = (PIMAGE_NT_HEADERS)((DWORD_PTR)hModule+ dosHeaders->e_lfanew);
+    HANDLE tmpHookAddr = NULL;
+
+    // Check if the dll is x86 or x64
+    if( ntHeaders->FileHeader.Machine ==  IMAGE_FILE_MACHINE_I386)
+    {
+        // Only supporst 64 bit dll's
+        printf("Invalid DLL version only supports x64\n");
+        return -1;
+    } 
+    SIZE_T dllImageSize = ntHeaders->OptionalHeader.SizeOfImage;
+
+    // resolve import address table
+    PIMAGE_IMPORT_DESCRIPTOR importDescriptor = NULL;
+    IMAGE_DATA_DIRECTORY importsDirectory = ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT];
+    importDescriptor = (PIMAGE_IMPORT_DESCRIPTOR)(importsDirectory.VirtualAddress + (DWORD_PTR)hModule);
+    LPCSTR libraryName = "";
+    HMODULE library = NULL;
+    HANDLE hookOffset = -1;
+    while (importDescriptor->Name != NULL)
+    {
+//        printf("ImportDescriptor (%llX)\n", importDescriptor);
+        libraryName = (LPCSTR)importDescriptor->Name + (DWORD_PTR)hModule;
+        printf("LibraryName = %s\n", libraryName);
+        if( strcmp(libraryName, "USER32.dll") == 0)
+        {
+            hookOffset = importDescriptor->FirstThunk;
+            break;
+        }
+        importDescriptor++;
+    }
+    if( hookOffset == -1) 
+    {
+        printf("Hook not found\n");
+        return;
+    }
+    tmpHookAddr = (DWORD_PTR)hModule + (DWORD_PTR)hookOffset;
+    printf("Found MessageBoxA (%llX) or (%llX)\n", hookOffset, tmpHookAddr);
+    PIMAGE_THUNK_DATA thunk = (PIMAGE_THUNK_DATA)tmpHookAddr;
     ptrHook = thunk->u1.Function;
     thunk->u1.Function = &myFunction;
 }
