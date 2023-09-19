aea8d5709ddf9455b09a21598b4990b1e6206e0f
Scott Nusbaum <snus@local.com>
2023-09-19 13:28:23 -0400

    Changed the shellcode for the queueuserapc to just spawn calc rather than a reverse shell.

--- techniques/injectionTechniques/queueuserapc/poc/build.sh
+++ techniques/injectionTechniques/queueuserapc/poc/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-x86_64-w64-mingw32-gcc queueuserapc.c -w -masm=intel -fpermissive -static -lpsapi -Wl,--subsystem,console -o q.exe
+x86_64-w64-mingw32-gcc queueuserapc.c -w -masm=intel -fpermissive -static -lpsapi -Wl,--subsystem,console -o ../bin/queueuserapc.exe
