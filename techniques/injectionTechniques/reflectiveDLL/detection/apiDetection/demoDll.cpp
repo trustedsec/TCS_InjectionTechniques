3874ebc9542a661c0432df008a384c4ff4240595
cyberGoatPsyOps <leo@leobastidas.com>
2023-09-27 18:52:30 -0500

    update reflective dll

--- techniques/injectionTechniques/reflectiveDLL/detection/apiDetection/demoDll.cpp
+++ techniques/injectionTechniques/reflectiveDLL/detection/apiDetection/demoDll.cpp
@@ -1,68 +1,72 @@
+// Code taken modified and taken from https://blog.securehat.co.uk/process-injection/detecting-process-injection-using-microsoft-detour-hooks
+
 #include <windows.h>
 #include "detours.h"
 
-//BOOL loadLibraryCalled = FALSE;
-//BOOL virtualAllocCalled = FALSE;
-//BOOL getProcAddressCalled = FALSE;
-//BOOL createRemoteThreadCalled = FALSE;
-//BOOL memcpyCalled = FALSE;
-//BOOL readProcessMemoryCalled = FALSE;
-//BOOL createProcessCalled = FALSE;
-//BOOL virtualProtectCalled = FALSE;
-//BOOL virtualAllocExCalled = FALSE;
-//BOOL writeProcessMemoryCalled = FALSE;
+BOOL loadLibraryCalled = FALSE; // detected
+BOOL virtualAllocCalled = FALSE; // detected
+BOOL getProcAddressCalled = FALSE; // detected
+//BOOL createRemoteThreadCalled = FALSE; // not detected
+//BOOL memcpyCalled = FALSE; // not detected
+//BOOL readProcessMemoryCalled = FALSE; // not detected
+BOOL createProcessCalled = FALSE; // detected
+//BOOL virtualProtectCalled = FALSE; // detected
+//BOOL virtualAllocExCalled = FALSE; // detected
+//BOOL writeProcessMemoryCalled = FALSE; // detected
 
 
 // Hooked API calls related to standard process injection
-//HMODULE(WINAPI* oLoadLibrary)(LPCSTR) = LoadLibraryA;
-//LPVOID(WINAPI* oVirtualAlloc)(LPVOID, SIZE_T, DWORD, DWORD) = VirtualAlloc;
-//FARPROC(WINAPI* oGetProcAddress)(HMODULE, LPCSTR) = GetProcAddress;
+HMODULE(WINAPI* oLoadLibrary)(LPCSTR) = LoadLibraryA;
+LPVOID(WINAPI* oVirtualAlloc)(LPVOID, SIZE_T, DWORD, DWORD) = VirtualAlloc;
+FARPROC(WINAPI* oGetProcAddress)(HMODULE, LPCSTR) = GetProcAddress;
 //void* (WINAPI* omemcpy)(void*, const void*, size_t) = memcpy;
 //HANDLE(WINAPI* oCreateRemoteThread)(HANDLE, LPSECURITY_ATTRIBUTES, SIZE_T, LPTHREAD_START_ROUTINE, LPVOID, DWORD, LPDWORD) = CreateRemoteThread;
 //BOOL(WINAPI* oReadProcessMemory)(HANDLE, LPCVOID, LPVOID, SIZE_T, SIZE_T*) = ReadProcessMemory;
-//BOOL(WINAPI* oCreateProcessA)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION) = CreateProcessA;
+BOOL(WINAPI* oCreateProcessA)(LPCSTR, LPSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCSTR, LPSTARTUPINFOA, LPPROCESS_INFORMATION) = CreateProcessA;
 //BOOL(WINAPI* oVirtualProtect)(LPVOID, SIZE_T, DWORD, PDWORD) = VirtualProtect;
 //LPVOID(WINAPI* oVirtualAllocEx)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD) = VirtualAllocEx;
 //BOOL(WINAPI* oWriteProcessMemory)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*) = WriteProcessMemory;
 
 
 // Our hook function for LoadLibrary
-//HMODULE WINAPI hookedLoadLibrary(LPCSTR lpLibFileName)
-//{
-//    loadLibraryCalled = TRUE;
-//    if (loadLibraryCalled) {
-//        //MessageBoxW(HWND_DESKTOP, L"Reflective injection detected (LoadLibrary). Killing Process.", L"Ghetto EDR", MB_OK);
-//        //DWORD killPid = GetCurrentProcessId();
-//        //HANDLE hKillProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, killPid);
-//        //TerminateProcess(hKillProcess, 0);
+HMODULE WINAPI hookedLoadLibrary(LPCSTR lpLibFileName)
+{
+    loadLibraryCalled = TRUE;
+//    if (loadLibraryCalled && virtualAllocCalled && getProcAddressCalled) {
+//        MessageBoxW(HWND_DESKTOP, L"Reflective injection detected. Killing Process.", L"Ghetto EDR", MB_OK);
+//        DWORD killPid = GetCurrentProcessId();
+//        HANDLE hKillProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, killPid);
+//        TerminateProcess(hKillProcess, 0);
 //    }
-//    return oLoadLibrary(lpLibFileName);
-//}
+    return oLoadLibrary(lpLibFileName);
+}
 
 // Our hook function for VirtualAlloc
-//LPVOID WINAPI hookedVirtualAlloc(LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect)
-//{
-//    virtualAllocCalled = TRUE;
-//    if (virtualAllocCalled && getProcAddressCalled && loadLibraryCalled) {
-//        //MessageBoxW(HWND_DESKTOP, L"Reflective injection detected(VirtualAlloc & getProcAddress & LoadLibrary). Killing Process.", L"Ghetto EDR", MB_OK);
-//        //DWORD killPid = GetCurrentProcessId();
-//        //HANDLE hKillProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, killPid);
-//        //TerminateProcess(hKillProcess, 0);
+LPVOID WINAPI hookedVirtualAlloc(LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect)
+{
+    virtualAllocCalled = TRUE;
+//    if (virtualAllocCalled && loadLibraryCalled) {
+//        MessageBoxW(HWND_DESKTOP, L"Reflective injection detected. Killing Process.", L"Ghetto EDR", MB_OK);
+//        DWORD killPid = GetCurrentProcessId();
+//        HANDLE hKillProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, killPid);
+//        TerminateProcess(hKillProcess, 0);
 //    }
-//    return oVirtualAlloc(lpAddress, dwSize, flAllocationType, flProtect);
-//}
+    return oVirtualAlloc(lpAddress, dwSize, flAllocationType, flProtect);
+}
 
-//FARPROC WINAPI hookedGetProcAddress(HMODULE hModule, LPCSTR lpProcName)
-//{
-//    getProcAddressCalled = TRUE;
-//    if (getProcAddressCalled && virtualAllocCalled && loadLibraryCalled) {
-//        //MessageBoxW(HWND_DESKTOP, L"Reflective injection detected(GetProcAddress & VirtualAlloc & LoadLibrary). Killing Process.", L"Ghetto EDR", MB_OK);
-//        //DWORD killPid = GetCurrentProcessId();
-//        //HANDLE hKillProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, killPid);
-//        //TerminateProcess(hKillProcess, 0);
+// Our hook function for GetProcAddress
+FARPROC WINAPI hookedGetProcAddress(HMODULE hModule, LPCSTR lpProcName)
+{
+    getProcAddressCalled = TRUE;
+//    if (getProcAddressCalled) {
+//        MessageBoxW(HWND_DESKTOP, L"Reflective injection detected(GetProcAddress). Killing Process.", L"Ghetto EDR", MB_OK);
+//        DWORD killPid = GetCurrentProcessId();
+//        HANDLE hKillProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, killPid);
+//        TerminateProcess(hKillProcess, 0);
 //    }
-//    return oGetProcAddress(hModule, lpProcName);
-//}
+    return oGetProcAddress(hModule, lpProcName);
+}
+
 // Our hook function for memcpy
 //void* hookedMemcpy(void* dest, const void* src, size_t count)
 //{
@@ -83,9 +87,9 @@
 //    createRemoteThreadCalled = TRUE;
 //    if (CreateRemoteThread) {
 //        MessageBoxW(HWND_DESKTOP, L"Reflective injection detected(CreateRemoteThread). Killing Process.", L"Ghetto EDR", MB_OK);
-//        //DWORD killPid = GetCurrentProcessId();
-//        //HANDLE hKillProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, killPid);
-//        //TerminateProcess(hKillProcess, 0);
+//        DWORD killPid = GetCurrentProcessId();
+//        HANDLE hKillProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, killPid);
+//        TerminateProcess(hKillProcess, 0);
 //    }
 //    return oCreateRemoteThread(hProcess, lpThreadAttributes, dwStackSize, lpStartAddress, lpParameter, dwCreationFlags, lpThreadId);
 //}
@@ -104,19 +108,19 @@
 //}
 
 // our hook function for createProcess
-//BOOL WINAPI hookedCreateProcessA(LPCSTR lpApplicationName, LPSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes, LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles,
-//    DWORD dwCreationFlags, LPVOID lpEnvironment, LPCSTR lpCurrentDirectory, LPSTARTUPINFOA lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation)
-//{
-//    createProcessCalled = TRUE;
-//    if (createProcessCalled) {
-//        MessageBoxW(HWND_DESKTOP, L"Reflective injection detected(CreateProcessA). Killing Process.", L"Ghetto EDR", MB_OK);
-//        //DWORD killPid = GetCurrentProcessId();
-//        //HANDLE hKillProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, killPid);
-//        //TerminateProcess(hKillProcess, 0);
-//    }
-//    return oCreateProcessA(lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, bInheritHandles,
-//        dwCreationFlags, lpEnvironment, lpCurrentDirectory, lpStartupInfo, lpProcessInformation);
-//}
+BOOL WINAPI hookedCreateProcessA(LPCSTR lpApplicationName, LPSTR lpCommandLine, LPSECURITY_ATTRIBUTES lpProcessAttributes, LPSECURITY_ATTRIBUTES lpThreadAttributes, BOOL bInheritHandles,
+    DWORD dwCreationFlags, LPVOID lpEnvironment, LPCSTR lpCurrentDirectory, LPSTARTUPINFOA lpStartupInfo, LPPROCESS_INFORMATION lpProcessInformation)
+{
+    createProcessCalled = TRUE;
+    if (createProcessCalled && loadLibraryCalled && virtualAllocCalled && getProcAddressCalled) {
+        MessageBoxW(HWND_DESKTOP, L"Reflective injection detected (the best detection logic on earth, look at source code to find out). Killing Process.", L"Ghetto EDR", MB_OK);
+        DWORD killPid = GetCurrentProcessId();
+        HANDLE hKillProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, killPid);
+        TerminateProcess(hKillProcess, 0);
+    }
+    return oCreateProcessA(lpApplicationName, lpCommandLine, lpProcessAttributes, lpThreadAttributes, bInheritHandles,
+        dwCreationFlags, lpEnvironment, lpCurrentDirectory, lpStartupInfo, lpProcessInformation);
+}
 
 // Our hook function for VirtualProtect
 //BOOL WINAPI hookedVirtualProtect(LPVOID lpAddress, SIZE_T dwSize, DWORD flNewProtect, PDWORD lpflOldProtect)
@@ -169,13 +173,13 @@ BOOL APIENTRY DllMain( HMODULE hModule,
         // Apply our hooks
         DetourTransactionBegin();
         DetourUpdateThread(GetCurrentThread());
-        //DetourAttach(&(PVOID&)oLoadLibrary, hookedLoadLibrary);
-        //DetourAttach(&(PVOID&)oVirtualAlloc, hookedVirtualAlloc);
-        //DetourAttach(&(PVOID&)oGetProcAddress, hookedGetProcAddress);
+        DetourAttach(&(PVOID&)oLoadLibrary, hookedLoadLibrary);
+        DetourAttach(&(PVOID&)oVirtualAlloc, hookedVirtualAlloc);
+        DetourAttach(&(PVOID&)oGetProcAddress, hookedGetProcAddress);
         //DetourAttach(&(PVOID&)omemcpy, hookedMemcpy);
         //DetourAttach(&(PVOID&)oCreateRemoteThread, hookedCreateRemoteThread);
         //DetourAttach(&(PVOID&)oReadProcessMemory, hookedReadProcessMemory);
-        //DetourAttach(&(PVOID&)oCreateProcessA, hookedCreateProcessA);
+        DetourAttach(&(PVOID&)oCreateProcessA, hookedCreateProcessA);
         //DetourAttach(&(PVOID&)oVirtualProtect, hookedVirtualProtect);
         //DetourAttach(&(PVOID&)oVirtualAllocEx, hookedVirtualAllocEx);
         //DetourAttach(&(PVOID&)oWriteProcessMemory, hookedWriteProcessMemory);
